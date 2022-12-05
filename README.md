d97d57474faa2877d0521a8e72d3f0960bfeba15
Omkar Bhalerao <opb7@cornell.edu>
2022-12-05 00:14:50 -0500

    Day 5 complete

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Omkar's attempt (keyword attempt) to solve problems after midnight EST quickly f
 | 2 | 3:59 | 141 | 6:36 (2:37) | 133 |
 | 3 | 3:20 | 137 | 6:03 (2:43) | 148 |
 |4| 2:17| 119 | 4:59 (2:42) | 409 |
+|5| 10:07 | 480 | 11:27 (1:20) | 384 |
 ## notes
 
 Day 4: Slight bug in code which locked me out for a minute :(
---
+++ day05/Day05B.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+lines = [l.rstrip() for l in open("../input.txt").readlines()]
+
+idx = 0
+nlines = []
+while lines[idx] != '':
+    nlines.append(lines[idx])
+    idx+=1
+
+nlines.reverse()
+n = 0
+stacks = []
+for idx1, line in enumerate(nlines):
+    if idx1 == 0:
+        n = int(line.split()[-1])
+        stacks = [deque() for i in range(n)]
+    else:
+        cnt = 0
+        idxx=0
+        while idxx < len(line):
+            if line[idxx+1].isalpha():
+                stacks[cnt].append(line[idxx+1])
+            idxx+=4
+            cnt+=1
+
+idx+=1
+while idx < len(lines):
+    line = lines[idx].split()
+    a = int(line[1])
+    b = int(line[3])-1
+    c = int(line[5])-1
+    lst = []
+    for i in range(a):
+        lst.append(stacks[b].pop())
+    lst.reverse()
+    for x in lst:
+        stacks[c].append(x)
+    idx+=1
+
+print(''.join(i[-1] for i in stacks))
